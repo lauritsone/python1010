c684dbc14f67c1f933c8c8000e8a7c71043d694e
Thomas <mail.lauritsen@pm.me>
2024-11-18 13:29:32 +0100

    endret slik at tom linje printes med /n

--- Pris_El_Bensin_v01.py
+++ Pris_El_Bensin_v01.py
@@ -39,20 +39,16 @@ KostnadForskjell =  KostnadBensin - KostnadElbil
 print("Det er",KostnadForskjell,"kr forskjell pr år mellom bensin og elbil")
 
 if KostnadBensin > KostnadElbil:
-    print("Elbil er rimeligst med en totalkost pr år på",KostnadElbil,"kr")
-    print("         ")
-    print("Dette består årskostnaden av:")
-    print("         ")
+    print("Elbil er rimeligst med en totalkost pr år på",KostnadElbil,"kr",end="\n\n")
+    print("Dette består årskostnaden av:",end="\n\n")
     print("Trafikkforsikringsavgift:",round(Trafikkforsikringsavgift,1),"kr")
     print("Kostnad drivstoff:",DrivstoffbrukEl,"kr")
     print("Bomavgift", BomavgiftEl,"kr")
     print("Forsikring",ForsikringEL,"kr")
     
 elif KostnadBensin < KostnadElbil:
-        print("Bensin er billigst med en totalkost pr år på",KostnadBensin,"kr")
-        print("         ")
-        print("Dette består årskostnaden av:")
-        print("         ")
+        print("Bensin er billigst med en totalkost pr år på",KostnadBensin,"kr",end="\n\n")
+        print("Dette består årskostnaden av:",end="\n\n")
         print("Trafikkforsikringsavgift:",round(Trafikkforsikringsavgift,1),"kr")
         print("Kostnad drivstoff:",DrivstofforbrukBensin,"kr")
         print("Bomavgift", BomavgiftBensin,"kr")
